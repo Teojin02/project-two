77ce55ab3fa7a4969c4000de34756408ffbacbf0
Your Name <you@example.com>
2020-10-17 23:42:39 -0700

    update

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 const express = require("express");
 const session = require("express-session");
 // Requiring passport as we've configured it
-const passport = require("./config/passport");
+const passport = require("./Sequelize-Passport-Example/config/passport");
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
-const db = require("./models");
+const db = require("./Sequelize-Passport-Example/models");
 
 // Creating express app and configuring middleware needed for authentication
 const app = express();
@@ -21,8 +21,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Requiring our routes
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+require("./Sequelize-Passport-Example/routes/html-routes.js")(app);
+require("./Sequelize-Passport-Example/routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(() => {
